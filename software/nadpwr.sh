b40d17825c4c5d1b31f357083e327d0e385d738b
Frederic Pierson <github@garatronic.fr>
2017-11-22 18:43:35 +0100

    MAJ nadpwr.sh

--- software/nadpwr.sh
+++ software/nadpwr.sh
@@ -4,10 +4,20 @@
 # 
 
 POWER_KEY_GPIO=26
+NADHAT_PWR=0
 
-gpio -g mode $POWER_KEY_GPIO out
-gpio -g write $POWER_KEY_GPIO 1
-echo pulse low PWRKEY pin  on GPIO$POWER_KEY_GPIO for 1s to startup SIM800...
-sleep 1
-gpio -g write $POWER_KEY_GPIO 0
+# Just check if NadHAT is not already powered
+gpio mode 15 in
+NADHAT_PWR=`gpio read 15`
+gpio mode 15 alt0
+
+if [ $NADHAT_PWR -eq 0 ]; then
+	gpio -g mode $POWER_KEY_GPIO out
+	gpio -g write $POWER_KEY_GPIO 1
+	echo pulse low PWRKEY pin on GPIO$POWER_KEY_GPIO for 1s to startup SIM800...
+	sleep 1
+	gpio -g write $POWER_KEY_GPIO 0
+else
+	echo SIM800C is already powered.
+fi
 echo end
